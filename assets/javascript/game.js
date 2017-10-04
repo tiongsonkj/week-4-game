c4d251f926755c023353109a1a49c8681a25f5a1
Kelvin Tiongson <kelvintiongson@kelvins-mbp-2.attlocal.net>
2017-10-03 19:34:18 -0500

    week4Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,23 +7,28 @@ $("#targetNumber").html(targetNumber);
 // create a function that will update the wins/losses?
 
 // created 4 unique crystal values and put them in an array
-var crystalValue1 = 1 + Math.floor(Math.random() * 12);
-var crystalValue2 = 1 + Math.floor(Math.random() * 12);
-var crystalValue3 = 1 + Math.floor(Math.random() * 12);
-var crystalValue4 = 1 + Math.floor(Math.random() * 12);
-var crystalArray = [crystalValue1, crystalValue2, crystalValue3, crystalValue4];
-console.log(crystalArray);
+function getCrystalValue () {
+	// going 1 ID in a time and putting value in it
+for(var i = 1; i <= 4; i++) {
+	$("#crystal-" + i).attr("data-crystalvalue",1 + Math.floor(Math.random() * 12));
+}
 
-// HOW CAN I GET DIFFERENT IMAGES WITH THE CRYSTAL VALUES?!?!?!
+}
+
 // for loop that will create 4 different images and will give them 
 // each a different value.
+// THE LOOPS HAVE DIFFERENT IDS
 for (var i = 0; i < 4; i++) {
 	var crystal = $("<img>");
 	crystal.addClass("crystal-image");
-	crystal.attr("src", "assets/images/greenCrystal.jpg");
-	crystal.attr("data-crystalvalue", crystalArray[i]);
+	// when you concatenate a string with a number, it just adds to the string.
+	crystal.attr("src", "assets/images/Crystal"+(i+1)+".jpg");
+	crystal.attr("id", "crystal-"+(i+1));
 	$("#crystals").append(crystal);
 }
+
+// calling values to the elements on the page
+getCrystalValue();
 
 // function: when image is clicked, provide it with the random
 // crystal Value and keep adding it and alert it to the page.
@@ -38,6 +43,8 @@ $(".crystal-image").on("click", function() {
 		alert("You win!");
 		wins++;
 		resetGame();
+		$("#gameStats").html("<p>You won!</p>" + 
+			"<p>Wins: </p>" + wins + "<p>Losses: </p>" + losses);
 		console.log("This is the new target: " + targetNumber);
 		// call function reset game which will... 
 		// bring counter back to zero, get a new target number,
@@ -47,6 +54,8 @@ $(".crystal-image").on("click", function() {
 		alert("You lose!");
 		losses++;
 		resetGame();
+		$("#gameStats").html("<p>You lost!</p>" + 
+			"<p>Wins: </p>" + wins + "<p>Losses: </p>" + losses);
 		console.log("This is the new target: " + targetNumber);
 		// call function reset game which will... 
 		// bring counter back to zero, get a new target number,
@@ -62,10 +71,13 @@ function resetGame() {
 
 	// get a new target number
 	// working but not replacing it in HTML. WHY?!??!?!
+	// redisplayed this to the page
 	targetNumber = getNewTargetNumber();
+	$("#targetNumber").html(targetNumber);
 
 	// get new array values
-	// no idea how to start
+	// calling to reassign values to elements on the page
+	getCrystalValue();
 }
 
 function getNewTargetNumber() {
